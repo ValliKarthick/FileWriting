41547edf0f80c12ea3781ff4795207bb5e45db9f
vallinannu <vallinannu@LAPTOP-GIVRBI76>
2023-03-24 21:28:09 +0530

    Add project files.

---
+++ FileWriting/FileOperation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace FileWriting
+{
+    public class FileOperation
+    {
+        string filePath = @"C:\Users\vallinannu\source\repos\FileWriting\Testfile.txt";
+        string copyPath = @"C:\Users\vallinannu\source\repos\FileWriting\MyCopyFile.txt";
+        public void CreateFileUsingStreamWriter()
+        {
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter streamWriter = File.CreateText(filePath))
+                {
+                    streamWriter.WriteLine("Third Run..");
+                    streamWriter.WriteLine("Wait..");
+                    streamWriter.WriteLine("Third Run..");
+                    streamWriter.WriteLine("Wait..");
+                }
+            }
+        }
+        public void ReadFileUsingStreamReader()
+        {
+            using (StreamReader streamReader = File.OpenText(filePath))
+            {
+                string fileContents;
+                while ((fileContents = streamReader.ReadLine()) == "Process Completed")
+                {
+                    Console.WriteLine(fileContents);
+                }
+            }
+        }
+        public void PerformFileReadAllText()
+        {
+            //string writeText = "Hello World!";
+            //File.WriteAllText(filePath, writeText);
+
+            string readText = File.ReadAllText(filePath);
+            Console.WriteLine(readText);
+        }
+        public void PerformReadAllLines()
+        {
+            String[] output = File.ReadAllLines(filePath);
+
+            Console.WriteLine(output[0]);
+            Console.WriteLine(output[1]);
+        }
+        public void CopyFile()
+        {
+            string sourcePath = filePath;
+
+            string destinationPath = copyPath;
+
+            //File.Copy(filePath, copyPath);
+
+            if (File.Equals(filePath, copyPath))
+            {
+                Console.WriteLine("Yes..");
+            }
+            else Console.WriteLine("No..");
+        }
+    }
+}
---
+++ FileWriting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FileWriting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileOperation fileOperation = new FileOperation();
+            //fileOperation.CreateFileUsingStreamWriter();
+            //fileOperation.ReadFileUsingStreamReader();
+            //fileOperation.PerformFileReadAllText();
+            //fileOperation.PerformReadAllLines();
+            fileOperation.CopyFile();
+
+            //Console.Write("Enter Account No : ");
+            //int accountNumber = Convert.ToInt32(Console.ReadLine()); 
+            //Console.Write("Enter Account Holder Name : ");
+            //string accountHolderName = Console.ReadLine();
+            //Console.Write("Enter Bank Name : ");
+            //string bankName = Console.ReadLine();
+            //Console.Write("Enter Bank Balance : ");
+            //int accountBalance = Convert.ToInt32(Console.ReadLine());
+
+            //string fullPath = "C:/Users/vallinannu/source/repos/FileWriting/Output.csv";
+            //string outPut = string.Format("{0},{1},{2},{3}\n",accountNumber.ToString(), accountHolderName, bankName, accountBalance.ToString());
+            
+            //File.WriteAllText(fullPath, outPut);
+
+            //Console.WriteLine("Bank Informationwritten to file successfully.");
+            //Console.WriteLine("The file output.csv should contain the bank information");
+            //string readText = File.ReadAllText(fullPath);
+            //Console.WriteLine(readText);
+            Console.ReadLine();
+        }
+    }
+}
